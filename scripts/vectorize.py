49397c8abde416e46102075cc1620b6a40b3a040
Vojtech Mikes <vojtechmikes98@gmail.com>
2024-10-17 14:17:43 +0200

    Added vectors, but i think only two are correct

--- scripts/vectorize.py
+++ scripts/vectorize.py
@@ -2,8 +2,52 @@ import pandas as pd
 
 dataset = pd.read_parquet("results/dataset.parquet")
 
-dataset.info()
+grouped_dataset = dataset.groupby("fullbarcode", as_index=True).mean(
+    numeric_only=True
+)
 
-grouped_dataset = dataset.groupby("fullbarcode").mean(numeric_only=True)
+grouped_dataset = grouped_dataset.dropna()
 
-print(grouped_dataset.head())
+measurement_vectors = grouped_dataset[
+    [
+        "pdi",
+        "digital_biomass",
+        "greenness_average",
+        "leaf_area_index",
+        "light_pene_depth",
+        "ndvi_average",
+        "npci_average",
+        "psri_average",
+    ]
+]
+
+state_vector = grouped_dataset[
+    [
+        "max_temp",
+        "avg_temp",
+        "min_temp",
+        "max_humi",
+        "avg_humi",
+        "min_humi",
+    ]
+]
+
+control_vector = grouped_dataset[
+    [
+        "max_temp",
+        "avg_temp",
+        "min_temp",
+        "max_humi",
+        "avg_humi",
+        "min_humi",
+    ]
+]
+
+
+measurement_vectors.to_parquet("results/measurement_v.parquet")
+control_vector.to_parquet("results/control_v.parquet")
+state_vector.to_parquet("results/state_v.parquet")
+
+measurement_vectors.to_excel("results/measurement_v.xlsx")
+control_vector.to_excel("results/control_v.xlsx")
+state_vector.to_excel("results/state_v.xlsx")
